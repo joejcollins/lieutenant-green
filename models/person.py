18739ee498325c1857aa0c866ea5aaeed7343d1e
Joe J Collins <joejcollins@users.noreply.github.com>
2022-08-23 17:29:57 +0000

    Pending changes exported from your codespace

--- models/person.py
+++ models/person.py
@@ -3,14 +3,24 @@ from datetime import datetime
 from pydantic import BaseModel
 
 
+class Hat(BaseModel):
+    color: str = "red"
+    size: int = 75
+    price: float = 10.0
+
+
 class Person(BaseModel):
-    time: datetime = datetime.now()
-    name: str = "Bart Simpson"
+    time: datetime = None
+    name: str = "El Barto"
     age: int = 12
 
     def __init__(self) -> None:
+        # Initialize the time otherwise it always returns the time the web app
+        # started.
         super().__init__()
         self.time = datetime.now()
+
+    hat: Hat = Hat()
 
 
 class PersonPlain():
