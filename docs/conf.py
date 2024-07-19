d07baa88fc65ea6ae0679aede5a31f119999b8b4
Joe J Collins <joejcollins@users.noreply.github.com>
2024-07-19 07:51:17 +0000

    Custom css

---
+++ docs/conf.py
@@ -0,0 +1,26 @@
+"""Configuration for Sphinx documentation using the Read the Docs theme."""
+
+# Project information
+project = "lieutentant-green"
+author = "Joe Collins"
+release = "1"
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",  # To include source code links
+    "sphinxcontrib.mermaid",
+]
+html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_sidebars = {
+    "**": [
+        "globaltoc.html",  # Main table of contents
+        "sourcelink.html",  # Source link to GitHub
+        "searchbox.html",  # Search box
+    ]
+}
+
+def setup(app):
+    """Set up so that we can add custom features."""
+    app.add_css_file('custom.css')
