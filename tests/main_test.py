5c8dd948f591629fc41dc20c0a86c25f8e4eae5e
Joe J Collins <joejcollins@users.noreply.github.com>
2024-12-30 16:47:04 +0000

    Rename actions

--- tests/main_test.py
+++ tests/main_test.py
@@ -4,7 +4,7 @@ from lieutenant_green import main
 
 
 def test_main_message() -> None:
-    """Confirm message returned."""
+    """Confirm that the message is returned."""
     the_message = main.main()
     assert "Hello" in the_message
     assert "world" in the_message
