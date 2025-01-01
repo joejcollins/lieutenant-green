6f7ee7381f6710b936cdbeba3b1b718efed17d87
Joe J Collins <joejcollins@users.noreply.github.com>
2025-01-01 19:16:41 +0000

    Mount the raw data

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,8 @@
 # Create the virtual environment.
 make venv-dev
 .venv/bin/pre-commit install
+
+# Mount the raw data
+echo $LIEUTENANT_GREEN_GCS_READONLY_KEY > /tmp/key.json
+mkdir ./data/raw
+gcsfuse --key-file=/tmp/key.json --implicit-dirs --o ro --cache-dir .cache 567866 ./data/raw
