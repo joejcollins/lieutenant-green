027d6341966103bc097e3152f8713692861f4dd7
Joe J Collins <joejcollins@users.noreply.github.com>
2025-01-01 20:30:39 +0000

    Change order

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,4 +1,4 @@
 # Mount the raw data
-echo $LIEUTENANT_GREEN_GCS_READONLY_KEY > /tmp/key.json
 mkdir --parents ./data/raw
+echo $LIEUTENANT_GREEN_GCS_READONLY_KEY > /tmp/key.json
 gcsfuse --key-file=/tmp/key.json --implicit-dirs --o ro --cache-dir .cache 567866 ./data/raw
