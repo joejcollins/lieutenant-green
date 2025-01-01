c628481830071427097304bd7e15a35d6c84e57f
Joe J Collins <joejcollins@users.noreply.github.com>
2025-01-01 20:53:36 +0000

    Test

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,4 +1,5 @@
 # Mount the raw data
+whoami
 mkdir --parents ./data/raw
-printf "%s" $LIEUTENANT_GREEN_GCS_READONLY_KEY > /tmp/key.json
+echo "$LIEUTENANT_GREEN_GCS_READONLY_KEY" | jq '.' > /tmp/key.json
 gcsfuse --key-file=/tmp/key.json --implicit-dirs --o ro --cache-dir .cache 567866 ./data/raw
