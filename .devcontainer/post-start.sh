da3579fad01980be927e6208f48422a478009ceb
Joe J Collins <joejcollins@users.noreply.github.com>
2025-01-01 22:10:50 +0000

    Remove sync

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,5 +1,4 @@
 # Mount the raw data
 mkdir --parents ./data/raw
 echo $LIEUTENANT_GREEN_GCS_READONLY_KEY > /tmp/key.json
-sync
 gcsfuse --key-file=/tmp/key.json --implicit-dirs --o ro --cache-dir .cache 567866 ./data/raw
