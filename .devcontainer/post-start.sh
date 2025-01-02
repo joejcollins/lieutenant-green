6225e9c6b7d01ce6920b77c709da682b4a520362
Joe J Collins <joejcollins@users.noreply.github.com>
2025-01-02 05:28:39 +0000

    Use sed

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,7 +1,7 @@
 # Mount the raw data
 mkdir --parents ./data/raw
-echo $LIEUTENANT_GREEN_GCS_READONLY_KEY > key1.json
-echo $LIEUTENANT_GREEN_GCS_READONLY_KEY | jq -c . > key2.json
-echo $LIEUTENANT_GREEN_GCS_READONLY_KEY | tr -d '\r\n' > key3.json
-printf %s $LIEUTENANT_GREEN_GCS_READONLY_KEY > key4.json
-# gcsfuse --key-file=/tmp/key.json --implicit-dirs --o ro --cache-dir .cache 567866 ./data/raw
+# Pipe the key via sed because when CodeSpaces runs this script the encoding gets messed
+# up. This isn't necessary in the terminal once the CodeSpace has started but I don't
+# know why.  Something to do with encoding that is beyond me.
+echo $LIEUTENANT_GREEN_GCS_READONLY_KEY | sed ':a;N;$!ba;s/\n/\\n/g' > /tmp/key.json
+gcsfuse --key-file=/tmp/key.json --implicit-dirs --o ro --cache-dir .cache 567866 ./data/raw
