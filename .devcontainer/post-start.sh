b6267dac9e030abfe05c254bfe30d528284f01de
Joe J Collins <joejcollins@users.noreply.github.com>
2025-01-02 04:06:37 +0000

    Test restart

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,4 +1,7 @@
 # Mount the raw data
 mkdir --parents ./data/raw
-echo $LIEUTENANT_GREEN_GCS_READONLY_KEY > /tmp/key.json
-gcsfuse --key-file=/tmp/key.json --implicit-dirs --o ro --cache-dir .cache 567866 ./data/raw
+echo $LIEUTENANT_GREEN_GCS_READONLY_KEY > key1.json
+echo $LIEUTENANT_GREEN_GCS_READONLY_KEY | jq -c . > key2.json
+echo $LIEUTENANT_GREEN_GCS_READONLY_KEY | tr -d '\r\n' > key3.json
+printf %s $LIEUTENANT_GREEN_GCS_READONLY_KEY > key4.json
+# gcsfuse --key-file=/tmp/key.json --implicit-dirs --o ro --cache-dir .cache 567866 ./data/raw
