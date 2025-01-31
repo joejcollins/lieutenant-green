3adff3b1cfbcd2ffe7dba637afb03f3d6d997aa0
Joe J Collins <joejcollins@users.noreply.github.com>
2025-01-31 15:25:33 +0000

    Mount the bucket

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -4,4 +4,4 @@ mkdir --parents ./data/raw
 # up. This isn't necessary in the terminal once the CodeSpace has started but I don't
 # know why.  Something to do with encoding that is beyond me.
 echo $LIEUTENANT_GREEN_GCS_READONLY_KEY | sed ':a;N;$!ba;s/\n/\\n/g' > /tmp/key.json
-gcsfuse --key-file=/tmp/key.json --implicit-dirs --o ro --cache-dir .cache 567866 ./data/raw
+gcsfuse --key-file=/tmp/key.json --implicit-dirs --o ro --cache-dir .cache lieutenant-green-zzz ./data/raw
